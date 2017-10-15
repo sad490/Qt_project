16706f3e380c5315e9588be6544fb554971aa3fa
sad490 <ztecube@gmail.com>
2017-10-15 10:29:23 +0800

    modified:   Practice.pro.user 	new file:   Practice.pro.user.8efdee3 	modified:   main.cpp

--- Practice/main.cpp
+++ Practice/main.cpp
@@ -1,11 +1,48 @@
+
+/**
+*So good 
+**/
+
 #include "mainwindow.h"
 #include <QApplication>
+#include <QLabel>
+#include <QTableView>
+#include <QLineEdit>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+
+
+void configure(QWidget& window)
+{
+    QLabel* name = new QLabel(QApplication::translate("topplace", "Name : "));
+    QLabel* value = new QLabel(QApplication::translate("topplace", "Value : "));
+    QLineEdit* edit = new QLineEdit();
+    QLineEdit* editvalue = new QLineEdit();
+    QTableView* table = new QTableView();
+
+    QHBoxLayout* editlayout = new QHBoxLayout();
+    editlayout->addWidget(name);
+    editlayout->addWidget(edit);
+    editlayout->addWidget(value);
+    editlayout->addWidget(editvalue);
+
+
+    QVBoxLayout* tablelayut = new QVBoxLayout();
+    tablelayut->addLayout(editlayout);
+    tablelayut->addWidget(table);
+
+    window.setLayout(tablelayut);
+    window.setWindowTitle(QApplication::translate("nestedLayout", "Nested Layout"));
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    QWidget window;
+
+    configure(window);
+
+    window.show();
 
     return a.exec();
 }
